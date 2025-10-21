47a6277b6d3c78e6bbbe306c581e6b708225d4a1
GwenNguyen <103728920+GwenNguyen2604@users.noreply.github.com>
2025-10-20 18:55:13 -0700

    refactored database_manager.py

--- backend/database/database_manager.py
+++ backend/database/database_manager.py
@@ -1,3 +1,6 @@
+"""
+    This module manages database
+"""
 import sqlite3
 import os
 
@@ -6,15 +9,27 @@ CARS_TABLE = "cars"
 MAINT_TABLE = "maint_cars"
 VALID_COLUMNS = {"brand", "model", "year", "vin"}
 
-# Name: CheckIfDatabaseExists
-# Description: Returns true if the database exists, false otherwise
-def CheckIfDatabaseExists():
+def check_if_database_exists():
+    """
+    Check if the database exists
+
+    Returns:
+        bool: if the database exists
+    """
     return os.path.exists(DB_FILE)
 
 
-# Name: WriteNewCarToTable
-# Description: Creates a new car entry to the table in the database
-def WriteNewCarToTable(brand_name, model_name, model_year, car_vin):
+def write_new_car_to_table(brand_name, model_name, model_year, car_vin):
+    """
+    Append new car entry to the cars table
+
+    Args:
+        brand_name (string) : Brand name of the car
+        model_name (string) : Car's model name
+        model_year (string?) : Model year
+        car_vin (string?) : Car VIN
+    """
+
     # create the values
     car_entries = (brand_name, model_name, model_year, car_vin)
     # Build the column
@@ -37,81 +52,144 @@ def WriteNewCarToTable(brand_name, model_name, model_year, car_vin):
     con.commit()
 
 
-# Name: GetAllCarsFromTable
-# Description: Gets the data of the cars in the database
-def GetAllCarsFromTable():
+def get_all_cars_from_table():
+    """
+    Print out all cars entries from teh cars table and maint_cars table
+
+    Returns:
+        list: All car entries from only the cars table
+    """
+
     con = sqlite3.connect(DB_FILE)
 
     # List of tables
     tables = [(CARS_TABLE, "*** cars table ***"),
               (MAINT_TABLE, "\n*** maint_cars table ***")]
-    car_data = {};
+    car_data = {}
 
     # Iterate through both maint and cars tables to print.
     # Return only data from cars table
     for table_name, header in tables:
-            print(header)
-            rows = con.execute(f"SELECT * FROM {table_name}").fetchall()
-            for row in rows:
-                print(row)
-            if table_name == CARS_TABLE:
-                car_data[table_name] = rows
+        print(header)
+        rows = con.execute(f"SELECT * FROM {table_name}").fetchall()
+        for row in rows:
+            print(row)
+        if table_name == CARS_TABLE:
+            car_data[table_name] = rows
 
     con.commit()
     return car_data
 
 
-# Name: MoveCarToMaintenanceTable
-# Description: Moves a car from the cars table to the maintenance table
-def MoveCarToMaintenanceTable(car_vin):
-    MoveCarBetweenTablesUtil(CARS_TABLE, MAINT_TABLE, car_vin)
+def move_car_to_maintenance_table(car_vin):
+    """
+    Move a specified car entry to the maint table
+
+    Args:
+        car_vin (string): The car VIN
+    """
+    move_car_between_tables_util(CARS_TABLE, MAINT_TABLE, car_vin)
 
 
-# Name: MoveMaintCarToCarsTable
-# Description: Moves a car from the maintenance table to the cars table
-def MoveMaintCarToCarsTable(car_vin):
-    MoveCarBetweenTablesUtil(MAINT_TABLE, CARS_TABLE, car_vin)
+def move_maint_car_to_cars_table(car_vin):
+    """
+    Move a specified car entry fromt the cars table to the maint table
+
+    Args:
+        car_vin (string): The car VIN
+    """
+    move_car_between_tables_util(MAINT_TABLE, CARS_TABLE, car_vin)
 
 
-# Name: GetAllVinsGivenBrand
-# Description: Returns a sql object of all vins given a brand parameter from the cars table
-def GetAllVinsGivenBrand(brand_name):
-    return GetAllVinsWithFilterUtil("brand", brand_name)
+def get_all_vins_given_brand(brand_name):
+    """
+    Get a sql object of all vins given the car's brand parameter from the cars table
+
+    Args:
+        brand_name (string): The car's brand
+
+    Returns:
+        the sql object of all vins
+    """
+    return get_all_vins_with_filter_util("brand", brand_name)
 
 
-# Name: GetAllVinsGivenModel
-# Description: Returns a sql object of all vins given a model parameter from the cars table
-def GetAllVinsGivenModel(model_name):
-    return GetAllVinsWithFilterUtil("model", model_name)
+def get_all_vins_given_model(model_name):
+    """
+    Get a sql object of all vins given a model parameter from the cars table
+
+    Args:
+        model_name (string): The car's model name
+
+    Returns:
+        the sql object of all vins
+    """
+    return get_all_vins_with_filter_util("model", model_name)
 
 
-# Name: GetAllVinsGivenYear
-# Description: Returns a sql object of all vins given a year parameter from the cars table
-def GetAllVinsGivenYear(year_num):
-    return GetAllVinsWithFilterUtil("year", year_num)
+def get_all_vins_given_year(year_num):
+    """
+    Get a sql object of all vins given a year parameter from the cars table
+
+    Args:
+        year_num (string): The car's year
+
+    Returns:
+        the sql object of all vins
+    """
+    return get_all_vins_with_filter_util("year", year_num)
 
 
-# Name: GetAllCarDataGivenBrand
-# Description: Returns a sql object of all car data given the brand from the cars table
-def GetAllCarDataGivenBrand(brand_name):
-    return GetAllCarsWithFilterUtil("brand", brand_name)
+def get_all_car_data_given_brand(brand_name):
+    """
+    Get a sql object of all car data given the brand from the cars table
+
+    Args:
+        brand_name (string): The car's brand name
+
+    Returns:
+        the sql object of all car data
+    """
+    return get_all_cars_with_filter_util("brand", brand_name)
 
 
-# Name: GetAllCarDataGivenModel
-# Description: Returns a sql object of all car data given the model from the cars table
-def GetAllCarDataGivenModel(model_name):
-    return GetAllCarsWithFilterUtil("model", model_name)
+def get_all_car_data_given_model(model_name):
+    """
+    Get a sql object of all car data given the model from the cars table
+
+    Args:
+        model_name (string): The car's model name
+
+    Returns:
+        the sql object of all car data
+    """
+    return get_all_cars_with_filter_util("model", model_name)
 
 
-# Name: GetAllCarDataGivenYear
-def GetAllCarDataGivenYear(year_num):
-    return GetAllCarsWithFilterUtil("year", year_num)
+def get_all_car_data_given_year(year_num):
+    """
+    Get a sql object of all car data given the year from the cars table
+
+    Args:
+        year_num (string): The car's year
+
+    Returns:
+        the sql object of all car data
+    """
+    return get_all_cars_with_filter_util("year", year_num)
 
 
-# Name: MoveCarBetweenTablesUtil
-# Description: Utillity function to move a car entry between tables. Used by
-#             MoveCarToMaintenanceTable and MoveMaintCarToCarsTable
-def MoveCarBetweenTablesUtil(source_table, dest_table, car_vin):
+def move_car_between_tables_util(source_table, dest_table, car_vin):
+    """
+    Utillity function to move a car entry between tables.
+    Used by MoveCarToMaintenanceTable and MoveMaintCarToCarsTable
+
+    Args:
+        source_table (table): The table where the data is from
+        dest_table (table): The table where it is being moved to
+        car_vin (string): car's VIN
+    """
+
     con = sqlite3.connect(DB_FILE)
 
     #check if the car exists in the source table
@@ -131,10 +209,20 @@ def MoveCarBetweenTablesUtil(source_table, dest_table, car_vin):
     con.commit()
 
 
-# Name: GetAllVinsWithFilterUtil
-# Description: Utillity function to get all VINs data given a filtering parameter
-#              Current Filters: brand_name, model_name, year
-def GetAllVinsWithFilterUtil(filter_field, filter_value):
+
+def get_all_vins_with_filter_util(filter_field, filter_value):
+    """
+    Utillity function to get all VINs data given a filtering parameter
+    Current Filters: brand_name, model_name, year
+
+    Args:
+        filter_field (string): The filtered field
+        filter_value (string): the value filtered
+
+    Returns:
+        the sql object of all VINs data
+    """
+
     con = sqlite3.connect(DB_FILE)
     all_cars = con.execute(f"SELECT vin FROM {CARS_TABLE} WHERE {filter_field} = ?",
                            (filter_value,))
@@ -142,12 +230,20 @@ def GetAllVinsWithFilterUtil(filter_field, filter_value):
     return all_cars
 
 
-# Name: GetAllCarsWithFilterUtil
-# Description: Utillity function to get all car data given a filtering parameter
-#              Current Filters: brand_name, model_name, year
-def GetAllCarsWithFilterUtil(filter_field, filter_value):
-    con = sqlite3.connect(DB_FILE)
+def get_all_cars_with_filter_util(filter_field, filter_value):
+    """
+    Utillity function to get all car data given a filtering parameter
+    Current Filters: brand_name, model_name, year
 
+    Args:
+        filter_field (string): The filtered field
+        filter_value (string): the value filtered
+
+    Returns:
+        the sql object of all car data
+    """
+
+    con = sqlite3.connect(DB_FILE)
     allCars = con.execute(f"SELECT * FROM {CARS_TABLE} WHERE {filter_field} = ?",
                           (filter_value,))
     con.commit()
